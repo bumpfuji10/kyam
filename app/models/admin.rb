1c8509277b3b6ba7eb47e35ecb523821568669a7
Yo Kamada <bumpfuji10@gmail.com>
2023-01-15 02:52:46 +0900

    delete: admin_controllerの消し忘れ

--- app/models/admin.rb
+++
@@ -1,9 +0,0 @@
-class Admin < ApplicationRecord
-  has_secure_password
-
-  def Admin.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-end
