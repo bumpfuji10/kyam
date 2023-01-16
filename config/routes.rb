5d866b419f37b77a50e47650cbc674920bfc6636
Yo Kamada <bumpfuji10@gmail.com>
2023-01-17 01:36:06 +0900

    add: articleモデルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'articles/new', to: 'articles#new'
   get 'articles', to: 'articles#index'
+  get 'articles/:id', to: 'articles#edit'
   root 'static_pages#home'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_15_165931) do
+ActiveRecord::Schema.define(version: 2023_01_16_163512) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
