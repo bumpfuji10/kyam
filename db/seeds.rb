4be0e4b84e8f9e985d27210bc2fbc70fb9ddb907
Yo Kamada <bumpfuji10@gmail.com>
2023-01-19 15:46:21 +0900

    add: 常にid_1の記事が参照されるようにした

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Article.create do |s|
+  s.id    = 1
+  s.title = "TestArticle_1"
+  s.content = "buraburabura"
+end
