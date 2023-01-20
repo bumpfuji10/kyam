fc831b8f0644fca4d44f1260aae35508d98df5a1
Yo Kamada <bumpfuji10@gmail.com>
2023-01-20 17:48:26 +0900

    add: issueのCRUDを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
-  get 'issue', to: 'static_pages#issue'
   post 'articles/new', to: 'articles#create'
   resources :articles
+  resources :issues
+  post 'issues/new', to: 'issues#create'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
-Article.create do |s|
-  s.id    = 1
-  s.title = "TestArticle_1"
+#Article.create do |s|
+#  s.id    = 1
+#  s.title = "TestArticle_1"
+#  s.content = "buraburabura"
+#end
+
+Issue.create do |s|
+  s.id = 1
+  s.title = "いいね機能を追加する"
   s.content = "buraburabura"
+  s.status = 2
 end
