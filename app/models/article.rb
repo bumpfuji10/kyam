db7292053f66a952bde4e51a4371e2c6b86cc450
Yo Kamada <bumpfuji10@gmail.com>
2023-03-30 07:37:31 +0900

    画像をリサイズできるようにしました

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
   has_rich_text :content
   validates :title, presence: true
-  has_many_attached :images
+  has_many_attached :images do |attachable|
+    attachable.variant :display, resize_to_limit: [500, 500]
+  end
 end
