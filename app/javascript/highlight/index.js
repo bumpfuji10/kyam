160e8f5eadf7a969c0b07fee48cb26b5586e2894
Yo Kamada <bumpfuji10@gmail.com>
2023-04-04 07:34:57 +0900

    index.js修正

--- app/javascript/highlight/index.js
+++ app/javascript/highlight/index.js
@@ -1,9 +1,22 @@
 import hljs from 'highlight.js'
 import 'highlight.js/styles/monokai-sublime.css';
 
+import ruby from 'highlight.js/lib/languages/ruby'
+hljs.registerLanguage('ruby', ruby);
+
+import erb from 'highlight.js/lib/languages/erb'
+hljs.registerLanguage('erb', erb);
+
+import bash from 'highlight.js/lib/languages/bash'
+hljs.registerLanguage('bash', bash);
+
+import javascript from 'highlight.js/lib/languages/javascript'
+hljs.registerLanguage('javascript', javascript);
+
 hljs.configure({ languages: ['ruby', 'bash', 'javascript'] })
 document.addEventListener('turbolinks:load', (event) => {
   document.querySelectorAll('pre').forEach((block) => {
     hljs.highlightBlock(block)
   })
 })
+
