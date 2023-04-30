7cba472471f27ec943b3c0cc0fdff8a464d7c602
Yo Kamada <bumpfuji10@gmail.com>
2023-04-30 16:25:38 +0900

    add: 公開状態カラムはfalseをデフォルトとし、null許容しない

--- db/migrate/20230430072148_add_is_published_to_articles.rb
+++ db/migrate/20230430072148_add_is_published_to_articles.rb
@@ -1,5 +1,5 @@
 class AddIsPublishedToArticles < ActiveRecord::Migration[6.1]
   def change
-    add_column :articles, :is_published, :boolean
+    add_column :articles, :is_published, :boolean, default: false, null: false
   end
 end
